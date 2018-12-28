588f3dc26af3053aa47cd00cd45a4aad1d0eb352
Jonathan <jalmeida0291@gmail.com>
2018-12-27 22:31:34 -0500

    server will now redirect index ejs for unknown routes

--- index.js
+++ index.js
@@ -15,4 +15,6 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => res.render("index"));
 
+app.get("*", (req, res) => res.render("index"));
+
 app.listen(port, () => console.log(`Server has started on port ${port}`));
